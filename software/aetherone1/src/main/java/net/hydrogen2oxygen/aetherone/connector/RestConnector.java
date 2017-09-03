49838467a2e573ce9cbcf9722d084c067cd6ea9c
radionics <sciencediscovery2012@hotmail.com>
2017-09-03 20:09:13 +0200

    Save Target in RestController

--- software/aetherone1/src/main/java/net/hydrogen2oxygen/aetherone/connector/RestConnector.java
+++ software/aetherone1/src/main/java/net/hydrogen2oxygen/aetherone/connector/RestConnector.java
@@ -1,8 +1,12 @@
 package net.hydrogen2oxygen.aetherone.connector;
 
 import net.hydrogen2oxygen.aetherone.hotbits.HotbitsClient;
+import net.hydrogen2oxygen.aetherone.peristence.dao.TargetRepository;
+import net.hydrogen2oxygen.aetherone.peristence.jpa.Target;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
@@ -13,6 +17,9 @@ public class RestConnector {
 
     @Autowired
     private HotbitsClient hotbitsClient;
+
+    @Autowired
+    private TargetRepository targetRepository;
 
     @RequestMapping("ping")
     public String ping() throws IOException {
@@ -26,5 +33,9 @@ public class RestConnector {
         return result;
     }
 
+    @RequestMapping(value = "territory", method = RequestMethod.POST, consumes = "application/json")
+    public void saveNewTarget(@RequestBody Target newTarget) {
 
+        targetRepository.save(newTarget);
+    }
 }
