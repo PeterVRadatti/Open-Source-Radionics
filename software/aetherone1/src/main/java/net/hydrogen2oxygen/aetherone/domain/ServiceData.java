48b17127d6e8c0cee795e440ddb57a6d68ce5cff
radionics <sciencediscovery2012@hotmail.com>
2018-07-07 16:49:51 +0200

    Version 0006 fixed

--- software/aetherone1/src/main/java/net/hydrogen2oxygen/aetherone/domain/ServiceData.java
+++
@@ -1,8 +0,0 @@
-package net.hydrogen2oxygen.aetherone.domain;
-
-import lombok.Data;
-
-@Data
-public class ServiceData {
-
-}
--- software/aetherone1/src/main/java/net/hydrogen2oxygen/aetherone/hotbits/IHotbitsFactory.java
+++
@@ -1,9 +0,0 @@
-package net.hydrogen2oxygen.aetherone.hotbits;
-
-import java.io.File;
-import java.io.IOException;
-
-public interface IHotbitsFactory {
-
-	public File createHotbitPackage(int packageSize, String targetFolder) throws IOException;
-}
